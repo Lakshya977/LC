27262f19e03cd7e3a9605b9b49593acff512a865
Lakshya <Lakshya8998@gmail.com>
2025-07-28 21:58:50 +0530

    1315

--- wordladder.java
+++ wordladder.java
@@ -1,5 +1,7 @@
 public class wordladder {
-    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+    }
     
     public static void main(String[] args) {
         
