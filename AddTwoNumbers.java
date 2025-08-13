ab12e8cf91d369a37541af3134f1e2e0cfe68520
Lakshya <Lakshya8998@gmail.com>
2025-08-13 23:53:22 +0530

    2 Leetcode

---
+++ AddTwoNumbers.java
@@ -0,0 +1,22 @@
+public class AddTwoNumber {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode dummy = ans;
+        int carry=0;
+        while(l1!=null || l2!=null|| carry!=0){
+            int sum = carry;
+            if(l1!= null){
+                sum += l1.val;
+                l1= l1.next;
+            }
+            if(l2!=null){
+                sum+= l2.val;
+                l2=l2.next;
+            }
+            carry = sum /10;
+            dummy.next = new ListNode(sum%10);
+            dummy = dummy.next;
+        }
+        return ans.next;
+    }
+}
